79bac4520562f9f55efcb96bbbd983627a241449
m1guelzin <miguelgarridodev@gmail.com>
2024-08-21 10:56:58 -0300

    Aula 21-08

---
+++ Minha Primeira API/src/server.js
@@ -0,0 +1,4 @@
+// Importar a instância do Express configurada em index.js
+const app = require("./index");
+// Inicia o servidor na porta 5000, neste caso a Api será acessivel em http://localhost:5000/
+app.listen(5000);
